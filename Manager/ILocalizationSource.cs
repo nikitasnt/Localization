fdd8b0477c3c03e39850b1abec22ad466d8f5451
nikitasnt <14nikita14m@gmail.com>
2024-06-22 17:46:13 +0400

    Localization manager public interface definition.

---
+++ Manager/ILocalizationSource.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Localization.Manager;
+
+/// <summary>
+/// Source of localization strings. Allows you to get a localized version of a string from some storage.
+/// </summary>
+public interface ILocalizationSource
+{
+    /// <summary>
+    /// Get localized string by localization code and culture information.
+    /// </summary>
+    /// <param name="localizationCode">Localization code.</param>
+    /// <param name="cultureInfo">Culture information.</param>
+    public string this[string localizationCode, CultureInfo cultureInfo] { get; }
+
+    /// <summary>
+    /// Try to get localized string by localization code and culture information
+    /// </summary>
+    /// <param name="localizationCode">Localization code.</param>
+    /// <param name="cultureInfo">Culture information.</param>
+    /// <param name="result">Localized string.</param>
+    /// <returns><see langword="true"/> if the value was in storage; otherwise <see langword="false"/>.</returns>
+    public bool TryGetLocalizedString(string localizationCode, CultureInfo cultureInfo, out string result);
+}
--- Manager/LocalizationManager.cs
+++ Manager/LocalizationManager.cs
@@ -1,6 +1,39 @@
-﻿namespace Localization.Manager;
+﻿using System.Globalization;
 
+namespace Localization.Manager;
+
+/// <summary>
+/// String localization manager. Allows you to register string localization sources <see cref="ILocalizationSource"/>
+/// and receive localized versions of strings from these sources.
+/// </summary>
 public class LocalizationManager
 {
+    public string GetString(string localizationCode, CultureInfo? cultureInfo = null)
+    {
+        if (cultureInfo == null)
+        {
+            cultureInfo = CultureInfo.CurrentCulture;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public bool TryGetString(string localizationCode, out bool result)
+    {
+        var cultureInfo = CultureInfo.CurrentCulture;
+
+        throw new NotImplementedException();
+    }
     
+    public bool TryGetString(string localizationCode, CultureInfo cultureInfo, out string result)
+    {
+        ArgumentNullException.ThrowIfNull(cultureInfo);
+
+        throw new NotImplementedException();
+    }
+
+    public LocalizationManager RegisterSource(ILocalizationSource localizationSource)
+    {   
+        throw new NotImplementedException();
+    }
 }
